50cc13b4ab5197b7b08ba4844c3abd21230b8073
kol0M <62647546+kol0M@users.noreply.github.com>
2021-10-11 22:40:18 +0200

    all files added

---
+++ script.js
@@ -0,0 +1,15 @@
+function click1(){
+    document.getElementById('jpg').innerHTML = "<img src='alfa_romeo_159.jpg'> <br> alfa romeo na lawecie";
+}
+function click2(){
+    document.getElementById('jpg').innerHTML = "<img src='opel_astra.jpg'> opel astra boży samochód";
+}
+function click3(){
+    document.getElementById('jpg').innerHTML = "<img src='seat_ibiza.jpg'> samochód majkelowy";
+}
+function click4(){
+    document.getElementById('jpg').innerHTML = "<img src='bmw_e36.jpg'> śmietnik";
+}
+function click5(){
+    document.getElementById('jpg').innerHTML = "<img src='skoda_frosta_octavia.jpg'> frost sobie jedzie i marek też";
+}
